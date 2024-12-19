a1c3071433cf25ad6106b8dfc333982c8f7588d4
Tsukuyomi <NiklsGG@yandex.ru>
2024-12-19 10:53:32 +0400

    Изменение цвета для фраз "Infinite Tsukuyomi де/активированно"

--- src/main/java/com/infinity/tsukuyomi/MoonEyeItem.java
+++ src/main/java/com/infinity/tsukuyomi/MoonEyeItem.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.text.TextColor;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
@@ -22,7 +23,10 @@ public class MoonEyeItem extends Item {
                 // Отключаем Infinity Tsukuyomi
                 InfinityTsukuyomi.isTsukuyomiActive = false;
 
-                player.sendMessage(Text.literal("Infinite Tsukuyomi деактивирован!"), true);
+                player.sendMessage(
+                        Text.literal("Infinite Tsukuyomi").styled(style -> style.withColor(TextColor.fromRgb(0xFF0000))) // Красный цвет
+                                .append(Text.literal(" деактивирован!").styled(style -> style.withColor(TextColor.fromRgb(0x00FF00)))), // Зеленый цвет
+                        true);
             } else {
                 // Активируем Infinity Tsukuyomi
                 InfinityTsukuyomi.isTsukuyomiActive = true;
@@ -30,7 +34,10 @@ public class MoonEyeItem extends Item {
                 // Установить ночь
                 serverWorld.setTimeOfDay(15000);
 
-                player.sendMessage(Text.literal("Infinite Tsukuyomi активирован!"), true);
+                player.sendMessage(
+                        Text.literal("Infinite Tsukuyomi").styled(style -> style.withColor(TextColor.fromRgb(0xFF0000))) // Красный цвет
+                                .append(Text.literal(" активирован!").styled(style -> style.withColor(TextColor.fromRgb(0x00FF00)))), // Зеленый цвет
+                        true);
             }
         }
         return TypedActionResult.success(player.getStackInHand(hand));
