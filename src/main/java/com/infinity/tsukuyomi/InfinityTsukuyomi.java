7fdbb07237223d443b95a33fd05be44c64823dd0
Tsukuyomi <NiklsGG@yandex.ru>
2024-12-20 17:22:20 +0400

    Основной класс мода, в котором 30-38 строчка вызывает вопросы и сомнения в том, нужны ли они вообще

--- src/main/java/com/infinity/tsukuyomi/InfinityTsukuyomi.java
+++ src/main/java/com/infinity/tsukuyomi/InfinityTsukuyomi.java
@@ -1,6 +1,11 @@
 package com.infinity.tsukuyomi;
 
+import com.infinity.tsukuyomi.entity.ModEntities;
+import com.infinity.tsukuyomi.item.MoonEyeItem;
+import com.infinity.tsukuyomi.render.MadaraRenderer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -22,5 +27,14 @@ public class InfinityTsukuyomi implements ModInitializer {
 		Registry.register(Registries.ITEM, new Identifier(MOD_ID, "moon_eye"), Moon_eye);
 
 		LOGGER.info("Infinity Tsukuyomi Mod initialized!");
+
+		// Регистрируем рендерер
+		EntityRendererRegistry.register(ModEntities.MADARA, MadaraRenderer::new);
+
+		// Регистрируем слой модели с текстурой для Мадары
+		EntityModelLayerRegistry.registerModelLayer(
+				MadaraRenderer.MADARA_LAYER,
+				MadaraRenderer::createModelData
+		);
 	}
 }
