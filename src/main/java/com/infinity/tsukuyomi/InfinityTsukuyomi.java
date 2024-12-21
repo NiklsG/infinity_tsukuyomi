8f5a8be8a0a3ee0772a5135b871dc368bc6e8fdb
Tsukuyomi <NiklsGG@yandex.ru>
2024-12-21 21:17:38 +0400

    Основной класс мода, добавлено: регистрация креативной вкладки, регистрацию сущностей, регистрация моделей и регистрация предметов

--- src/main/java/com/infinity/tsukuyomi/InfinityTsukuyomi.java
+++ src/main/java/com/infinity/tsukuyomi/InfinityTsukuyomi.java
@@ -1,5 +1,6 @@
 package com.infinity.tsukuyomi;
 
+import com.infinity.tsukuyomi.item.ModCreativeTabs;
 import com.infinity.tsukuyomi.entity.ModEntities;
 import com.infinity.tsukuyomi.item.MoonEyeItem;
 import com.infinity.tsukuyomi.render.MadaraRenderer;
@@ -13,28 +14,35 @@ import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static com.infinity.tsukuyomi.item.ModItems.MADARA_SPAWN_EGG;
+
 public class InfinityTsukuyomi implements ModInitializer {
 
 	public static final String MOD_ID = "infinity_tsukuyomi";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static boolean isTsukuyomiActive = false;
-
 	public static final Item Moon_eye = new MoonEyeItem(new Item.Settings().maxCount(1));
 
 	@Override
 	public void onInitialize() {
-		// Регистрируем предмет
+		// Регистрация креативной вкладки
+		ModCreativeTabs.registerCreativeTab();
+
+		// Регистрация предметов
 		Registry.register(Registries.ITEM, new Identifier(MOD_ID, "moon_eye"), Moon_eye);
+		Registry.register(Registries.ITEM, new Identifier(MOD_ID, "madara_spawn_egg"), MADARA_SPAWN_EGG);
 
-		LOGGER.info("Infinity Tsukuyomi Mod initialized!");
+		// Регистрация сущностей
+		ModEntities.registerEntities();
 
-		// Регистрируем рендерер
+		// Регистрация рендера сущностей
 		EntityRendererRegistry.register(ModEntities.MADARA, MadaraRenderer::new);
 
-		// Регистрируем слой модели с текстурой для Мадары
+		// Регистрация модели сущности
 		EntityModelLayerRegistry.registerModelLayer(
 				MadaraRenderer.MADARA_LAYER,
 				MadaraRenderer::createModelData
 		);
+		LOGGER.info("Infinity Tsukuyomi Mod initialized!");
 	}
 }
